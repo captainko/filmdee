4d1f2bc27b294782569810c1600fd0792ba56b58
CaptainLee <17521098@gm.uit.edu.vn>
2019-02-19 19:02:23 +0700

    fix app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { SharedModule } from '@shared/shared.module';
-=======
-import { SharedModule } from './shared/shared.module';
-import { CoreModule } from './core/core.module';
-
-
-
->>>>>>> 8af841e395b18e3c637ab63567f58d06736ff498
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,11 +14,6 @@ import { CoreModule } from './core/core.module';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-<<<<<<< HEAD
-=======
-    CoreModule,
-    FormsModule
->>>>>>> 8af841e395b18e3c637ab63567f58d06736ff498
   ],
   providers: [],
   bootstrap: [AppComponent]

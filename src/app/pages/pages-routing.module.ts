4222a8634c90dc65d604aeb43abc7c2b18157cd3
CaptainLee <17521098@gm.uit.edu.vn>
2019-02-19 18:25:27 +0700

    Update Routing

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HomeModule } from './home/home.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'home'},
+  {path: 'home', loadChildren: () => HomeModule }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

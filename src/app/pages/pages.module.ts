1d5dfe7ef8f5350f949fd62d4dd3b1ef7b36235f
CaptainLee <17521098@gm.uit.edu.vn>
2019-02-19 18:28:54 +0700

    change directory for nav-bar

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
-import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-
+import { SharedModule } from '@shared/shared.module';
 @NgModule({
-  declarations: [HomeComponent, LoginComponent],
+  declarations: [],
   imports: [
     CommonModule,
+    SharedModule,
     PagesRoutingModule
   ]
 })

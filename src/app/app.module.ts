9494f1d4616b84b467791ce2d1deae36eadbe93f
CaptainLee <17521098@gm.uit.edu.vn>
2019-02-19 18:33:37 +0700

    update Imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from '@shared/shared.module';
-import { HeaderComponent } from './layouts/client-layout/header/header.component';
-import { FooterComponent } from './layouts/client-layout/footer/footer.component';
-import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
-import { FormsModule } from '@angular/forms';
-
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    HeaderComponent,
-    FooterComponent,
-    LoginComponent
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/layouts/client-layout/client-layout.module.ts
+++ src/app/layouts/client-layout/client-layout.module.ts
@@ -3,11 +3,22 @@ import { CommonModule } from '@angular/common';
 import { ClientLayoutComponent } from './client-layout.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { NavBarComponent } from './header/nav-bar/nav-bar.component';
+import { ClientLayoutRoutingModule } from './client-layout-routing.module';
+
+import { SearchService } from '@services/search/search.service';
 
 @NgModule({
-  declarations: [ClientLayoutComponent, HeaderComponent, FooterComponent],
+  declarations: [
+    ClientLayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavBarComponent,
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    ClientLayoutRoutingModule,
+  ],
+  providers: [SearchService]
 })
 export class ClientLayoutModule { }

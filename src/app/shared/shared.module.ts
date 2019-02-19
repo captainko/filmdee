0a99d827578029e97ad40c61c9ea1acb5deda82c
CaptainLee <17521098@gm.uit.edu.vn>
2019-02-19 19:00:45 +0700

    change folder slideshow

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from "@angular/common/http";
+import { SlideShowComponent } from './components/slide-show/slide-show.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SlideShowComponent
+  ],
   imports: [
     CommonModule
   ],

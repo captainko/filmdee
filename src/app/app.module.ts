bb2e86419defa71490c751354564996c1ba0886d
TuanKhoi <17520648@gm.uit.edu.vn>
2019-02-19 14:26:12 +0700

    footer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from '@shared/shared.module';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
+
+
 
 @NgModule({
   declarations: [
@@ -13,7 +17,9 @@ import { SharedModule } from '@shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    CoreModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

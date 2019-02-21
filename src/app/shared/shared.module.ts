441f735a711791bc6e1099074a3aa7e08254c6c1
CaptainLee <17521098@gm.uit.edu.vn>
2019-02-21 11:48:40 +0700

    Create Box-card

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,18 +3,21 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from "@angular/common/http";
 import { SlideShowComponent } from './components/slide-show/slide-show.component';
+import { BoxCardComponent } from './components/cards/box-card/box-card.component';
 
 
 @NgModule({
   declarations: [
-    SlideShowComponent
+    SlideShowComponent,
+    BoxCardComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BoxCardComponent,
   ]
 })
 export class SharedModule { }

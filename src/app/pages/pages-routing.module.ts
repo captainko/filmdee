5da45561c54d6831fc5d7eb1a4debbefd4d268a0
thinhle0510 <thinhle0510>
2019-02-21 14:31:51 +0700

    Change Navbar

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HomeModule } from './home/home.module';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'home'},
-  {path: 'home', loadChildren: () => HomeModule }
+  {path: 'home', loadChildren: () => HomeModule },
+  {path: 'login', component: LoginComponent }
 ];
 
 @NgModule({

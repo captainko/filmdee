0617fa39cbdad641cded05193f8d244f640a17c2
TuanKhoi <17520648@gm.uit.edu.vn>
2019-02-21 15:50:12 +0700

    commit logo

--- src/app/layouts/client-layout/footer/footer.component.ts
+++ src/app/layouts/client-layout/footer/footer.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  logo = 'assets/images/logo-filmdee.png';
   constructor() { }
 
   ngOnInit() {
--- src/app/layouts/client-layout/header/nav-bar/nav-bar.component.ts
+++ src/app/layouts/client-layout/header/nav-bar/nav-bar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit {
-
+  logo = 'assets/images/logo-filmdee.png';
   constructor() { }
 
   ngOnInit() {

cd36d4bbaa95c5c24f944d8936761fb6d53e5aa6
TuanKhoi <17520648@gm.uit.edu.vn>
2019-02-23 12:04:27 +0700

    add search filter

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeModule } from './home/home.module';
 import { LoginComponent } from './login/login.component';
 import { TheaterModule } from './theater/theater.module'
+import { SearchModule } from './search/search.module';
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: 'home'},
-  {path: 'home', loadChildren: () => HomeModule },
-  {path: 'login', component: LoginComponent },
-  {path: 'theater',loadChildren: () => TheaterModule}
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', loadChildren: () => HomeModule },
+  { path: 'login', component: LoginComponent },
+  { path: 'theater', loadChildren: () => TheaterModule },
+  { path: 'search', loadChildren: () => SearchModule }
 ];
 
 @NgModule({

c3a2120e35d12b1460db886fcde23b837bd7e479
TuanKhoi <17520648@gm.uit.edu.vn>
2019-02-26 08:38:21 +0700

    cmt

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -5,7 +5,6 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-<<<<<<< HEAD
 export class SearchComponent implements OnInit {
 
   search = {
@@ -16,10 +15,6 @@ export class SearchComponent implements OnInit {
     prop5: 'Quốc gia',
     prop6: 'Năm phát hành',
   }
-=======
-export class SearchComponent implements OnInit, AfterViewInit {
-  show = false;
->>>>>>> 33335e49e99b6217deec84a273a8ff1aaaba2426
   constructor() { }
 
   ngOnInit() {
--- src/app/pages/search/search.module.ts
+++ src/app/pages/search/search.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
-=======
-import { SearchComponentModule } from "@shared/components/search/search.component.module";
->>>>>>> 33335e49e99b6217deec84a273a8ff1aaaba2426
 import { SearchRoutingModule } from './search-routing.module';
 import { SearchComponent } from './search.component';
 import { SearchResultComponent } from '@shared/components/search/search-result/search-result.component';

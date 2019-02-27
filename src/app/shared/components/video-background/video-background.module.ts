d38571df0f44158b9a403807f472f9b46900c015
Duc Duy <0374740747s@gmail.com>
2019-02-27 21:29:13 +0700

    Add detail Page play

--- src/app/shared/components/video-background/video-background.module.ts
+++ src/app/shared/components/video-background/video-background.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VideoBackgroundComponent } from './video-background.component';
+import { VideosModule } from '../videos/videos.module';
 
 @NgModule({
   declarations: [VideoBackgroundComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    VideosModule
   ],
   exports: [
     VideoBackgroundComponent

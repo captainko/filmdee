0b763d0deb303e047cd8952488b81fa71b3f50d3
CaptainLee <17521098@gm.uit.edu.vn>
2019-03-01 12:38:42 +0700

    changed search routing

--- src/app/pages/search/search.module.ts
+++ src/app/pages/search/search.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchRoutingModule } from './search-routing.module';
-import { SearchComponent } from './search.component';
+// import { SearchComponent } from './search.component';
 import { SearchResultModule } from '@shared/components/search/search-result/search-result.module';
 
 @NgModule({
-  declarations: [SearchComponent],
+  declarations: [
+    // SearchComponent
+  ],
   imports: [
     CommonModule,
     SearchRoutingModule,
+    SearchResultModule
+  ],
+  exports: [
     SearchResultModule
   ]
 })

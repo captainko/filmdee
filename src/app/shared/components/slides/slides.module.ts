80728ff061e9ecf09f6c3a6682facdb49baac78e
CaptainLee <17521098@gm.uit.edu.vn>
2019-03-02 16:47:53 +0700

    fix lazy load

--- src/app/shared/components/slides/slides.module.ts
+++ src/app/shared/components/slides/slides.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { OwlModule } from 'ngx-owl-carousel';
 import { SlideListComponent } from './slide-list/slide-list.component';
 import { CardsModule } from '../cards/cards.module';
-import { BoxCardComponent } from '../cards/box-card/box-card.component';
 import { LazyModule } from '@core/directives/lazy/lazy.module';
 import { LazyViewport } from "@core/directives/lazy/lazy-viewport";
+
+
 
 @NgModule({
   declarations: [SlideListComponent],
@@ -16,8 +17,7 @@ import { LazyViewport } from "@core/directives/lazy/lazy-viewport";
     LazyModule
   ],
   exports: [
-    SlideListComponent,
-    BoxCardComponent
+    SlideListComponent
   ]
 })
 export class SlidesModule { }

b30ec04ac5e2161e293fa93b957c1df31ef08c8d
ducmeohoang99 <leecatterduc99@gmail.com>
2019-03-20 08:40:18 +0700

    fix charts

--- src/app/pages/chart/chart.component.ts
+++ src/app/pages/chart/chart.component.ts
@@ -9,101 +9,228 @@ export class ChartComponent implements OnInit {
 
 
 
-  list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  categorysTime = [
+    { active: true, name: 'Ngày' },
+    { active: false, name: 'Tháng' },
+    { active: false, name: 'Năm' }
+  ]
+  categorys = [
+    { active: true, name: 'Phim lẻ' },
+    { active: false, name: 'Phim bộ' },
+    { active: false, name: 'Anime' }
+
+  ]
 
   topTenAnime = [
     {
       id: 1,
-      name: "One Piece",
-      img: 'assets/images/thumb-film/1.jpg',
-      content: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.'
+      name: 'A Madea Family Funeral',
+      img: 'https://firebasestorage.googleapis.com/v0/b/film-dee-aaf1a.appspot.com/o/maxresdefault.jpg?alt=media&token=26ed2521-0341-400e-b4cf-0f69310418d9',
+      content: 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.'
     },
     {
       id: 2,
-      name: 'A Madea Family Funeral',
-      img: 'assets/images/thumb-film/2.jpg',
-      content: 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.'
+      name: "One Piece",
+      img: 'https://firebasestorage.googleapis.com/v0/b/film-dee-aaf1a.appspot.com/o/2526-f7c3590d7508e4ef53ae4209f2f6c6a0.jpg?alt=media&token=0e9d884d-5019-4944-89f3-42df95248e78',
+      content: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.'
     },
     {
       id: 3,
       name: "Sex Education",
-      img: 'assets/images/thumb-film/3.jpg'
+      img: 'https://firebasestorage.googleapis.com/v0/b/film-dee-aaf1a.appspot.com/o/maxresdefault%20(1).jpg?alt=media&token=bede4cde-0b22-4d65-8a98-22a054cee653',
     },
     {
       id: 4,
       name: "The Flash",
-      img: 'assets/images/thumb-film/4.jpg'
-    },
-    {
-      id: 4,
-      name: "Naruto",
-      img: 'assets/images/thumb-film/15.jpg'
+      img: 'https://firebasestorage.googleapis.com/v0/b/film-dee-aaf1a.appspot.com/o/4bb22dd7c4ed2a93013049dd1a8ee2cc0b5dd5de.png?alt=media&token=2821e60d-846e-40fb-954d-ce7d0a16fdb1'
     },
     {
       id: 5,
-      name: "Rusia Da Sikva",
-      img: 'assets/images/thumb-film/5.jpg'
+      name: "Naruto",
+      img: 'https://firebasestorage.googleapis.com/v0/b/film-dee-aaf1a.appspot.com/o/preview.medium.jpg?alt=media&token=061f7e86-b4a1-46be-8a3a-3cc143746cac'
     },
     {
       id: 6,
-      name: "The Godfather",
-      img: 'assets/images/thumb-film/6.jpg'
+      name: "Rusia Da Sikva",
+      img: 'https://firebasestorage.googleapis.com/v0/b/film-dee-aaf1a.appspot.com/o/2526-f7c3590d7508e4ef53ae4209f2f6c6a0.jpg?alt=media&token=0e9d884d-5019-4944-89f3-42df95248e78'
     },
     {
       id: 7,
-      name: "Ashibo",
-      img: 'assets/images/thumb-film/7.jpg'
+      name: "The Godfather",
+      img: 'https://firebasestorage.googleapis.com/v0/b/film-dee-aaf1a.appspot.com/o/1_Qs9x0isKobtK3msvBafEpg.jpeg?alt=media&token=7ff6972a-9c2b-47fb-b25f-c13dc571b06a'
     },
     {
       id: 8,
-      name: "Cmifara",
-      img: 'assets/images/thumb-film/8.jpg'
+      name: "Ashibo",
+      img: 'https://firebasestorage.googleapis.com/v0/b/film-dee-aaf1a.appspot.com/o/maxresdefault%20(2).jpg?alt=media&token=19f6f86a-3736-4157-94e5-2292bdf0fc36'
     },
     {
       id: 9,
+      name: "Cmifara",
+      img: 'https://firebasestorage.googleapis.com/v0/b/film-dee-aaf1a.appspot.com/o/2526-f7c3590d7508e4ef53ae4209f2f6c6a0.jpg?alt=media&token=0e9d884d-5019-4944-89f3-42df95248e78'
+    },
+    {
+      id: 10,
       name: "Astrore",
-      img: 'assets/images/thumb-film/9.jpg'
+      img: 'https://firebasestorage.googleapis.com/v0/b/film-dee-aaf1a.appspot.com/o/2526-f7c3590d7508e4ef53ae4209f2f6c6a0.jpg?alt=media&token=0e9d884d-5019-4944-89f3-42df95248e78'
     },
   ]
 
   topTenSeries = [
     {
       id: 1,
-      name: "One Piece",
-      img: 'assets/images/thumb-film/1.jpg'
+      name: "Sex Education",
+      img: 'https://firebasestorage.googleapis.com/v0/b/film-dee-aaf1a.appspot.com/o/maxresdefault%20(1).jpg?alt=media&token=bede4cde-0b22-4d65-8a98-22a054cee653'
     },
     {
       id: 2,
       name: "One Piece",
-      img: 'assets/images/thumb-film/1.jpg'
+      img: 'https://firebasestorage.googleapis.com/v0/b/film-dee-aaf1a.appspot.com/o/2526-f7c3590d7508e4ef53ae4209f2f6c6a0.jpg?alt=media&token=0e9d884d-5019-4944-89f3-42df95248e78',
+      content: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.'
     },
     {
       id: 3,
-      name: "One Piece",
-      img: 'assets/images/thumb-film/1.jpg'
-    }
+      name: 'A Madea Family Funeral',
+      img: 'https://firebasestorage.googleapis.com/v0/b/film-dee-aaf1a.appspot.com/o/maxresdefault.jpg?alt=media&token=26ed2521-0341-400e-b4cf-0f69310418d9',
+      content: 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.'
+    },
+    {
+      id: 4,
+      name: "The Flash",
+      img: 'https://firebasestorage.googleapis.com/v0/b/film-dee-aaf1a.appspot.com/o/4bb22dd7c4ed2a93013049dd1a8ee2cc0b5dd5de.png?alt=media&token=2821e60d-846e-40fb-954d-ce7d0a16fdb1'
+    },
+    {
+      id: 5,
+      name: "Naruto",
+      img: 'https://firebasestorage.googleapis.com/v0/b/film-dee-aaf1a.appspot.com/o/preview.medium.jpg?alt=media&token=061f7e86-b4a1-46be-8a3a-3cc143746cac'
+    },
+    {
+      id: 6,
+      name: "Rusia Da Sikva",
+      img: 'https://firebasestorage.googleapis.com/v0/b/film-dee-aaf1a.appspot.com/o/2526-f7c3590d7508e4ef53ae4209f2f6c6a0.jpg?alt=media&token=0e9d884d-5019-4944-89f3-42df95248e78'
+    },
+    {
+      id: 7,
+      name: "The Godfather",
+      img: 'https://firebasestorage.googleapis.com/v0/b/film-dee-aaf1a.appspot.com/o/1_Qs9x0isKobtK3msvBafEpg.jpeg?alt=media&token=7ff6972a-9c2b-47fb-b25f-c13dc571b06a'
+    },
+    {
+      id: 8,
+      name: "Ashibo",
+      img: 'https://firebasestorage.googleapis.com/v0/b/film-dee-aaf1a.appspot.com/o/maxresdefault%20(2).jpg?alt=media&token=19f6f86a-3736-4157-94e5-2292bdf0fc36'
+    },
+    {
+      id: 9,
+      name: "Cmifara",
+      img: 'https://firebasestorage.googleapis.com/v0/b/film-dee-aaf1a.appspot.com/o/2526-f7c3590d7508e4ef53ae4209f2f6c6a0.jpg?alt=media&token=0e9d884d-5019-4944-89f3-42df95248e78'
+    },
+    {
+      id: 10,
+      name: "Astrore",
+      img: 'https://firebasestorage.googleapis.com/v0/b/film-dee-aaf1a.appspot.com/o/2526-f7c3590d7508e4ef53ae4209f2f6c6a0.jpg?alt=media&token=0e9d884d-5019-4944-89f3-42df95248e78'
+    },
   ]
-  
+
   topTenMovie = [
     {
       id: 1,
       name: "One Piece",
-      img: 'assets/images/thumb-film/1.jpg'
+      img: 'https://firebasestorage.googleapis.com/v0/b/film-dee-aaf1a.appspot.com/o/2526-f7c3590d7508e4ef53ae4209f2f6c6a0.jpg?alt=media&token=0e9d884d-5019-4944-89f3-42df95248e78',
+      content: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.'
     },
     {
       id: 2,
-      name: "One Piece",
-      img: 'assets/images/thumb-film/1.jpg'
+      name: 'A Madea Family Funeral',
+      img: 'https://firebasestorage.googleapis.com/v0/b/film-dee-aaf1a.appspot.com/o/maxresdefault.jpg?alt=media&token=26ed2521-0341-400e-b4cf-0f69310418d9',
+      content: 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.'
     },
     {
       id: 3,
-      name: "One Piece",
-      img: 'assets/images/thumb-film/1.jpg'
-    }
+      name: "Sex Education",
+      img: 'https://firebasestorage.googleapis.com/v0/b/film-dee-aaf1a.appspot.com/o/maxresdefault%20(1).jpg?alt=media&token=bede4cde-0b22-4d65-8a98-22a054cee653'
+    },
+    {
+      id: 4,
+      name: "The Flash",
+      img: 'https://firebasestorage.googleapis.com/v0/b/film-dee-aaf1a.appspot.com/o/4bb22dd7c4ed2a93013049dd1a8ee2cc0b5dd5de.png?alt=media&token=2821e60d-846e-40fb-954d-ce7d0a16fdb1'
+    },
+    {
+      id: 5,
+      name: "Naruto",
+      img: 'https://firebasestorage.googleapis.com/v0/b/film-dee-aaf1a.appspot.com/o/preview.medium.jpg?alt=media&token=061f7e86-b4a1-46be-8a3a-3cc143746cac'
+    },
+    {
+      id: 6,
+      name: "Rusia Da Sikva",
+      img: 'https://firebasestorage.googleapis.com/v0/b/film-dee-aaf1a.appspot.com/o/2526-f7c3590d7508e4ef53ae4209f2f6c6a0.jpg?alt=media&token=0e9d884d-5019-4944-89f3-42df95248e78'
+    },
+    {
+      id: 7,
+      name: "The Godfather",
+      img: 'https://firebasestorage.googleapis.com/v0/b/film-dee-aaf1a.appspot.com/o/1_Qs9x0isKobtK3msvBafEpg.jpeg?alt=media&token=7ff6972a-9c2b-47fb-b25f-c13dc571b06a'
+    },
+    {
+      id: 8,
+      name: "Ashibo",
+      img: 'https://firebasestorage.googleapis.com/v0/b/film-dee-aaf1a.appspot.com/o/maxresdefault%20(2).jpg?alt=media&token=19f6f86a-3736-4157-94e5-2292bdf0fc36'
+    },
+    {
+      id: 9,
+      name: "Cmifara",
+      img: 'https://firebasestorage.googleapis.com/v0/b/film-dee-aaf1a.appspot.com/o/2526-f7c3590d7508e4ef53ae4209f2f6c6a0.jpg?alt=media&token=0e9d884d-5019-4944-89f3-42df95248e78'
+    },
+    {
+      id: 10,
+      name: "Astrore",
+      img: 'https://firebasestorage.googleapis.com/v0/b/film-dee-aaf1a.appspot.com/o/2526-f7c3590d7508e4ef53ae4209f2f6c6a0.jpg?alt=media&token=0e9d884d-5019-4944-89f3-42df95248e78'
+    },
   ]
   constructor() { }
 
   ngOnInit() {
   }
 
+  active(vl) {
+    for (let index = 0; index < this.categorysTime.length; index++) {
+      if (index != vl) {
+        this.categorysTime[index].active = false;
+      } else {
+        this.categorysTime[index].active = true;
+      }
+    }
+  }
+  active2(vl) {
+    for (let index = 0; index < this.categorys.length; index++) {
+      if (index != vl) {
+        this.categorys[index].active = false;
+      } else {
+        this.categorys[index].active = true;
+      }
+    }
+  }
+  styleList0(): any {
+    if (window.innerWidth < 992) {
+      if (this.categorys[0].active) {
+        return { display: 'block' };
+      }
+      return { display: 'none' };
+    }
+  }
+  styleList1(): any {
+    if (window.innerWidth < 992) {
+      if (this.categorys[1].active) {
+        return { display: 'block' };
+      }
+      return { display: 'none' };
+    }
+  }
+  styleList2(): any {
+    if (window.innerWidth < 992) {
+      if (this.categorys[2].active) {
+        return { display: 'block' };
+      }
+      return { display: 'none' };
+    }
+  }
 }
